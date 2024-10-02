64a636d245e570a93fc08cb305fec3872c31b6b4
Victorpsaar <victorpsaar@gmail.com>
2024-10-01 21:03:24 -0300

    Test: Criação de teste para a funcionalidade de comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +7,18 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve renderizar dois comentarios', () => {
+        render(<PostComment />);
+        const input = screen.getByTestId('area-text')
+        const btn = screen.getByTestId('btn-text')
+
+        fireEvent.change(input, {target: {value: 'Belo Batimovel'}})
+        fireEvent.click(btn)
+
+        fireEvent.change(input, {target: {value: 'Belissimo Batimovel'}})
+        fireEvent.click(btn)
+
+        expect(screen.getAllByTestId('comment')).toHaveLength(2)
+    })
 });
